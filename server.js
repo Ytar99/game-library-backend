9f5aa2ad3aaff4287903fa915ac849fef2e65006
Semyon Puchof <semyon@innova-labs.net>
2024-07-30 18:45:41 +0300

    update: add vercel config

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
-const port = 3101;
-const API_KEY = "0672c1bccd4a8e37980a99bbac698365a7b61775";
-const API_URL = "https://www.giantbomb.com/api";
+
+const dotenvConfig = dotenv.config();
+const { API_KEY, API_URL, PORT } = dotenvConfig.parsed;
 
 // Configure CORS
 app.use(cors());
@@ -13,6 +14,7 @@ app.use(cors());
 // Define a GET endpoint that requests data from a third-party API
 app.get("/api/game/:guid", async (req, res) => {
   try {
+    console.log(`Requesting data for game ${req.params.guid} from client ${req.ip}`);
     const guid = req.params.guid;
     const response = await axios.get(`${API_URL}/game/${guid}/?api_key=${API_KEY}&format=json`);
     res.json(response.data);
@@ -23,6 +25,6 @@ app.get("/api/game/:guid", async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT || 3101, () => {
+  console.log(`Server is running on port ${PORT || 3101}`);
 });
